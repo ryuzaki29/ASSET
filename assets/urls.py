3cb5cb727139150e4e1ac6c7c702864145f9f212
jlcunanan1 <jlcunanan1@up.edu.ph>
2026-05-18 01:10:09 +0800

    Updated User Landing and User Login. Created and implemented User Registration, User List, and User Profile

--- assets/urls.py
+++ assets/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("", views.index, name="landing"),
     path("asset/<int:asset_id>/", views.asset_detail, name="asset_detail"),
     path("list/", views.asset_list, name="asset_list"),
-    path("users/", views.user_list, name="user_list"),
+    path('users/', views.user_list, name='user_list'),
+    path('users/<int:user_id>/', views.user_profile, name='user_profile'),
+    path("users/register/", views.register_view, name="user_registration"),
 ]
