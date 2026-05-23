2c9545d13a637fd2bb6fda9a5839d7540726349b
jlcunanan1 <jlcunanan1@gmail.com>
2026-05-24 02:39:51 +0800

    updated settings.py to include roles and updated navigation bar

--- assets/roles/views.py
+++ assets/roles/views.py
@@ -5,27 +5,27 @@ from .models import Role
 # List all Roles
 class RoleListView(ListView):
     model = Role
-    template_name = 'role_list.html' 
+    template_name = 'roles/role_list.html' 
     context_object_name = 'roles'
 
 # Create a Role
 class RoleCreateView(CreateView):
     model = Role
     fields = ['name', 'description']
-    template_name = 'role_form.html'
+    template_name = 'roles/role_form.html'
     success_url = reverse_lazy('role_list')
 
 # Update a Role
 class RoleUpdateView(UpdateView):
     model = Role
     fields = ['name', 'description']
-    template_name = 'role_form.html'
+    template_name = 'roles/role_form.html'
     success_url = reverse_lazy('role_list')
 
 # Delete a Role
 class RoleDeleteView(DeleteView):
     model = Role
-    template_name = 'role_confirm_delete.html'
+    template_name = 'roles/role_confirm_delete.html'
     success_url = reverse_lazy('role_list')
 
 # Detail view for a Role
