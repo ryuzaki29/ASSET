5447c695aa8cd3225ee1ab7661c930d22f981c64
mdellazar-web <mdellazar@up.edu.ph>
2026-05-25 12:08:31 +0800

    Design user dashboard, edit page, and registration templates with modern styling

--- assets/urls.py
+++ assets/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path("asset/<int:asset_id>/", views.asset_detail, name="asset_detail"),
     path("list/", views.asset_list, name="asset_list"),
     path("users/", views.user_list, name="user_list"),
+    path('register/', views.register_view, name='register'),
+   path("users/<int:user_id>/edit/", views.user_edit, name="user_edit"), 
+   
 ]
--- assets/views.py
+++ assets/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .forms import CustomUserCreationForm
+from .forms import UserEditForm
 
 from .models import Asset
 
@@ -8,10 +11,19 @@ from .models import Asset
 def landing(request):
     return render(request, "assets/landing.html")
 
-# Added Registration Form
 def register_view(request):
-    form = UserCreationForm()
-    return render(request, "registration/user_registration.html", {"form":form})
+
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, "registration/user_registration.html", {"form": form})
 
 def index(request):
     return render(request, "assets/index.html")
@@ -29,4 +41,25 @@ def asset_detail(request, asset_id):
 
 
 def user_list(request):
-    return render(request, "assets/user_list.html")
+    users = User.objects.all()
+
+    context = {
+        "users": users
+    }
+
+    return render(request, "assets/user_list.html", context)
+
+def user_edit(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+
+    if request.method == "POST":
+        form = UserEditForm(request.POST, instance=user)
+
+        if form.is_valid():
+            form.save()
+            return redirect("assets:user_list")
+
+    else:
+        form = UserEditForm(instance=user)
+
+    return render(request, "assets/user_edit.html", {"form": form})
